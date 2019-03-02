174e4b672f3934fcfe8cc12bad3ffcffc39b59c6
OguzhanGuldamlasi <oguldamlasi@etu.edu.tr>
2019-03-02 18:54:50 +0300

    test covarages are %100

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -14,4 +14,37 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+  @Test
+  public void test1(){
+      MessageQueue mq=mock(MessageQueue.class);
+      c=new Compute(mq);
+      when(mq.size()).thenReturn(0);
+      assertEquals(-1,c.countNumberOfOccurrences("aliveli"));
+  }
+@Test
+    public void test2(){
+        MessageQueue mq=mock(MessageQueue.class);
+        c=new Compute(mq);
+        when(mq.size()).thenReturn(5);
+        when(mq.contains(anyString())).thenReturn(false);
+        assertEquals(0,c.countNumberOfOccurrences("aliveli"));
+    }
+    @Test
+    public void test3(){
+        MessageQueue mq=mock(MessageQueue.class);
+        c=new Compute(mq);
+        when(mq.size()).thenReturn(5);
+        when(mq.contains(anyString())).thenReturn(true);
+        when(mq.getAt(anyInt())).thenReturn("aliveli");
+        assertEquals(5,c.countNumberOfOccurrences("aliveli"));
+    }
+    @Test
+    public void test4(){
+        MessageQueue mq=mock(MessageQueue.class);
+        c=new Compute(mq);
+        when(mq.size()).thenReturn(5);
+        when(mq.contains(anyString())).thenReturn(true);
+        when(mq.getAt(anyInt())).thenReturn("aliveli");
+        assertEquals(0,c.countNumberOfOccurrences("alivelia"));
+    }
 }
--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -1,9 +1,10 @@
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TestUtil {
   Util c;
@@ -13,4 +14,29 @@ public class TestUtil {
 
   @Test
   public void example() { assertTrue(true); }
+  @Test
+  public void test1(){//test for 1 sized array
+    assertFalse(c.compute(1));
+  }
+  @Test
+  public void test2(){//test for even sized
+    assertFalse(c.compute(1,2,3,4));
+  }
+  @Test
+  public void test3(){//Checking the exception
+    try {
+      c.compute(0,0,0,0,0);
+    }
+    catch(Exception e){
+      assertEquals(e.getClass(),RuntimeException.class);
+    }
+  }
+  @Test
+  public void test4(){
+    assertTrue(c.compute(1,1,1,1,1));
+  }
+  @Test
+  public void test5(){
+    assertFalse(c.compute(2,4,6,8,7));
+  }
 }
